4e5e8cd614069609885a29a5f0cf100572eac6f5
Scott Bowler <scott.bowler@learningfuze.com>
2018-06-16 14:13:42 -0700

    Initial drag / touch open working

--- src/components/side_nav/drag_drop.js
+++ src/components/side_nav/drag_drop.js
@@ -2,19 +2,75 @@ import React, { Component } from 'react';
 import './drag_drop.css';
 
 class DragDrop extends Component {
-    handleDrag(e) {
-        console.log('Dragging!', e.screenX);
+    constructor(props){
+        super(props);
+
+        this.handleDragStart = this.handleDragStart.bind(this);
+        this.handleDrag = this.handleDrag.bind(this);
+        this.handleDragEnd = this.handleDragEnd.bind(this);
+        this.handleTouchStart = this.handleTouchStart.bind(this);
+        this.handleTouchMove = this.handleTouchMove.bind(this);
+        this.handleTouchEnd = this.handleTouchEnd.bind(this);
     }
-    
-    handleDragStart(e){
+
+    handleDragStart(e) {
         console.log('Drag Start!', e.screenX);
+        this.start = e.screenX;
+        this.startTime = new Date().getTime();
     }
+
+    handleDrag(e) {
+        // console.log('Dragging!', e.screenX);
+        const pos = e.screenX;
+
+        if(pos > this.start){
+            this.props.slideOut(pos - this.start);
+        }
+    }
+
+    handleDragEnd(e){
+        console.log('Drag End:', e.screenX);
+        const timeElapsed = new Date().getTime() - this.startTime;
+        const distance = e.screenX - this.start
+        this.props.open(timeElapsed, distance);
+    }
+
+    handleTouchStart(e){
+        console.log('Touch Event:', e.targetTouches);
+        const touch = e.targetTouches[0];
+
+        this.start = touch.clientX;
+        this.startTime = new Date().getTime();
+    }
+
+    handleTouchMove(e){
+        // console.log('Moving:', e.targetTouches[0].clientX);
+        this.lastPos = e.targetTouches[0].clientX;
+
+        if (this.lastPos > this.start) {
+            this.props.slideOut(this.lastPos - this.start);
+        }
+    }
+
+    handleTouchEnd(e){
+        console.log('Drag End:', e);
+        const timeElapsed = new Date().getTime() - this.startTime;
+        const distance = this.lastPos - this.start
+        this.props.open(timeElapsed, distance);
+    }
+
     render(){
         return ( <div
             className="drag-target"
             draggable
+            onDragStart={this.handleDragStart}
             onDrag={this.handleDrag}
-            onDragStart={this.handleDragStart}></div>
+            onDragEnd={this.handleDragEnd}
+            onTouchStart={this.handleTouchStart}
+            onTouchMove={this.handleTouchMove}
+            onTouchEnd={this.handleTouchEnd}
+            // onTouchEndCapture={this.handleTouchEnd}
+            ></div>
         )
     }
 }
--- src/components/side_nav/index.js
+++ src/components/side_nav/index.js
@@ -5,20 +5,52 @@ import './side_nav.css';
 class SideNav extends Component {
     constructor(props){
         super(props);
+        this.width = 300;
+        this.offset = 5;
+        this.startPos = -100;
 
         this.state = {
-            pos: '-105%'
+            pos: this.startPos - this.offset,
+            transition: 0
         }
     }
+
+    slideOut(xPos){
+        if(this.state.pos >= 0) return;
+
+        let change = this.startPos + ((xPos / this.width) * 100);
+
+        if(change >= 0){
+            change = 0
+        }
+
+        this.setState({
+            pos: change
+        });
+    }
+
+    open(time, distance){
+        const totalDistance = this.width + this.offset;
+        const ppms = distance/time;
+        const remainingDistance = totalDistance - distance;
+        const transition =  remainingDistance / ppms;
+
+        this.setState({
+            pos: 0,
+            transition
+        });
+    }
+
     render(){
 
         const sideNavStyle = {
-            transform: `translate(${this.state.pos})`
+            transitionDuration: `${this.state.transition}ms`,
+            transform: `translate(${this.state.pos}%)`
         };
 
         return (
             <div>
-                <DragDrop />
+                <DragDrop open={this.open.bind(this)} slideOut={this.slideOut.bind(this)} />
                 <div style={sideNavStyle} className="side-nav">
                     <div className="top-section">
                         <span>L</span>
